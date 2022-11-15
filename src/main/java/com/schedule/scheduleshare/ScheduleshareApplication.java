56c1244b48e57f661edae7a2527138640c3e2baf
Hidenori Murai <hidenoweb822@gmail.com>
2022-11-15 11:25:32 +0900

    プロジェクト追加

---
+++ src/main/java/com/schedule/scheduleshare/ScheduleshareApplication.java
@@ -0,0 +1,13 @@
+package com.schedule.scheduleshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schedule/scheduleshare/ScheduleshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedule.scheduleshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
